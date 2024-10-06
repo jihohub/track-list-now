7f81fb2dea9b1c06c60679f6e38ce8510f3a009a
Jiho Park <diorpark@gmail.com>
2024-10-06 21:31:22 +0900

    Feat: 사용자가 구글 로그인 한 후, 해당 사용자의 공개 정보를 db에 추가하도록 수정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,34 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const existingUser = await prisma.user.findUnique({
+        where: { googleId: account.providerAccountId },
+      });
+
+      if (!existingUser) {
+        await prisma.user.create({
+          data: {
+            googleId: account.providerAccountId,
+            email: user.email,
+            name: user.name,
+            profileImage: user.image,
+          },
+        });
+      }
+      return true;
+    },
+
+    async session({ session, token, user }) {
+      const dbUser = await prisma.user.findUnique({
+        where: { email: session.user.email },
+      });
+
+      if (dbUser) {
+        session.user.id = dbUser.id;
+      }
+      return session;
+    },
+  },
 });
