60cb465ea9902ef4d7f61543e514e63c3208a7eb
Jiho Park <diorpark@gmail.com>
2024-10-25 16:43:14 +0900

    Feat: 검색 페이지 추가

---
+++ src/features/search/SearchResultItem.tsx
@@ -0,0 +1,88 @@
+import TImage from "@/features/common/TImage";
+import {
+  SearchResult,
+  SimplifiedAlbum,
+  SimplifiedArtist,
+  SimplifiedTrack,
+} from "@/types/search";
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+
+interface SearchResultItemProps {
+  result: SearchResult;
+  listType: "artist" | "track" | "album";
+}
+
+const SearchResultItem = ({ result, listType }: SearchResultItemProps) => {
+  const { t } = useTranslation("common");
+
+  // 타입 가드 함수 정의
+  const isArtist = (res: SearchResult): res is SimplifiedArtist => {
+    return (res as SimplifiedArtist).followers !== undefined;
+  };
+
+  const isTrack = (res: SearchResult): res is SimplifiedTrack => {
+    return (res as SimplifiedTrack).popularity !== undefined;
+  };
+
+  const isAlbum = (res: SearchResult): res is SimplifiedAlbum => {
+    return (res as SimplifiedAlbum).releaseDate !== undefined;
+  };
+
+  const getDetailPageUrl = () => {
+    if (listType === "artist") {
+      return `/artist/${(result as SimplifiedArtist).id}`;
+    }
+    if (listType === "track") {
+      return `/track/${(result as SimplifiedTrack).id}`;
+    }
+    if (listType === "album") {
+      return `/album/${(result as SimplifiedAlbum).id}`;
+    }
+    return "#";
+  };
+
+  return (
+    <li className="flex items-center justify-between text-sm text-white cursor-pointer bg-zinc-900 hover:bg-gray-700 p-2 rounded-lg">
+      <Link href={getDetailPageUrl()} passHref>
+        <div className="flex items-center">
+          <TImage
+            imageUrl={
+              listType === "artist"
+                ? isArtist(result)
+                  ? result.imageUrl
+                  : "/default-artist.png"
+                : listType === "track"
+                  ? isTrack(result)
+                    ? result.albumImageUrl
+                    : "/default-album.png"
+                  : isAlbum(result)
+                    ? result.imageUrl
+                    : "/default-album.png"
+            }
+            type={listType}
+            alt={result.name}
+            size="w-10 h-10"
+            className="mr-4 rounded-full"
+          />
+          <div className="flex flex-col">
+            <span>{result.name}</span>
+            {listType === "artist" && (
+              <span className="text-gray-400 text-xs">
+                {isArtist(result) && result.followers.toLocaleString()}{" "}
+                {t("followers")}
+              </span>
+            )}
+            {listType !== "artist" && (
+              <span className="text-gray-400 text-xs">
+                {isTrack(result) || isAlbum(result) ? result.artists : ""}
+              </span>
+            )}
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default SearchResultItem;
---
+++ src/features/search/SearchSection.tsx
@@ -0,0 +1,200 @@
+import NoResultsMessage from "@/features/search/NoResultsMessage";
+import SearchInput from "@/features/search/SearchInput";
+import SearchResultList from "@/features/search/SearchResultList";
+import SearchTabs from "@/features/search/SearchTabs";
+import {
+  SearchResponseData,
+  SimplifiedAlbum,
+  SimplifiedArtist,
+  SimplifiedTrack,
+} from "@/types/search";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+
+const SearchSection = () => {
+  const { t } = useTranslation(["common", "search"]);
+  const router = useRouter();
+  const { q, type } = router.query;
+
+  const [searchQuery, setSearchQuery] = useState<string>(
+    typeof q === "string" ? q : "",
+  );
+  const [currentType, setCurrentType] = useState<string>(
+    typeof type === "string" ? type : "artist,track,album",
+  );
+  const [hasSearched, setHasSearched] = useState<boolean>(false);
+  const resultsRef = useRef<HTMLUListElement>(null);
+
+  const fetchSearchResults = async ({ pageParam = 0 }) => {
+    const response = await axios.get<SearchResponseData>(
+      "/api/advanced-search",
+      {
+        params: {
+          q: searchQuery.trim(),
+          type: currentType,
+          limit: 10,
+          offset: pageParam,
+        },
+      },
+    );
+    return response.data;
+  };
+
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useInfiniteQuery({
+    queryKey: ["search", searchQuery, currentType],
+    queryFn: fetchSearchResults,
+    enabled: hasSearched && currentType !== "artist,track,album", // "All" 탭에서는 비활성화
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      if (currentType === "artist" && lastPage.artists?.next) {
+        return lastPage.artists.offset + lastPage.artists.limit;
+      }
+      if (currentType === "track" && lastPage.tracks?.next) {
+        return lastPage.tracks.offset + lastPage.tracks.limit;
+      }
+      if (currentType === "album" && lastPage.albums?.next) {
+        return lastPage.albums.offset + lastPage.albums.limit;
+      }
+      return undefined;
+    },
+  });
+
+  const handleSearch = () => {
+    if (!searchQuery.trim()) return;
+    refetch();
+    router.push({
+      pathname: "/search",
+      query: { q: searchQuery.trim(), type: currentType },
+    });
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  useEffect(() => {
+    if (resultsRef.current) {
+      resultsRef.current.scrollTop = 0;
+    }
+  }, [data]);
+
+  useEffect(() => {
+    // 페이지 로드 시 URL의 쿼리 스트링을 기반으로 검색 수행
+    if (q && type && typeof q === "string" && typeof type === "string") {
+      setHasSearched(true);
+      setSearchQuery(q.trim());
+      setCurrentType(type);
+      refetch();
+    }
+  }, [q, type, refetch]);
+
+  const placeholder = t("placeholder", { ns: "search" });
+  const searchButtonLabel = t("search", { ns: "search" });
+  const noResultMessage = t("no_result", { ns: "search" });
+
+  const tracks: SimplifiedTrack[] = [];
+  const artists: SimplifiedArtist[] = [];
+  const albums: SimplifiedAlbum[] = [];
+
+  data?.pages.forEach((page) => {
+    if (page.artists) {
+      artists.push(...page.artists.items);
+    }
+    if (page.tracks) {
+      tracks.push(...page.tracks.items);
+    }
+    if (page.albums) {
+      albums.push(...page.albums.items);
+    }
+  });
+
+  return (
+    <div className="bg-zinc-800 p-6 rounded-lg shadow-lg max-w-4xl w-full mx-auto">
+      <h2 className="text-xl font-semibold mb-4 text-white">
+        {t("search_for", { ns: "search" })}
+      </h2>
+
+      <SearchInput
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        handleSearch={handleSearch}
+        handleKeyPress={handleKeyPress}
+        placeholder={placeholder}
+        searchButtonLabel={searchButtonLabel}
+      />
+
+      <SearchTabs currentType={currentType} setCurrentType={setCurrentType} />
+
+      {hasSearched && currentType === "artist,track,album" && (
+        <>
+          {artists.length > 0 && (
+            <SearchResultList
+              searchResults={artists.slice(0, 5)}
+              listType="artist"
+              fetchMore={() => {}}
+              hasMore={false}
+            />
+          )}
+          {tracks.length > 0 && (
+            <SearchResultList
+              searchResults={tracks.slice(0, 5)}
+              listType="track"
+              fetchMore={() => {}}
+              hasMore={false}
+            />
+          )}
+          {albums.length > 0 && (
+            <SearchResultList
+              searchResults={albums.slice(0, 5)}
+              listType="album"
+              fetchMore={() => {}}
+              hasMore={false}
+            />
+          )}
+        </>
+      )}
+
+      {hasSearched && currentType !== "artist,track,album" && (
+        <SearchResultList
+          searchResults={
+            currentType.includes("artist")
+              ? artists
+              : currentType.includes("track")
+                ? tracks
+                : albums
+          }
+          listType={
+            currentType.includes("artist")
+              ? "artist"
+              : currentType.includes("track")
+                ? "track"
+                : "album"
+          }
+          fetchMore={fetchNextPage}
+          hasMore={hasNextPage || false}
+        />
+      )}
+
+      {hasSearched &&
+        tracks.length === 0 &&
+        artists.length === 0 &&
+        albums.length === 0 &&
+        !isLoading && <NoResultsMessage message={noResultMessage} />}
+    </div>
+  );
+};
+
+export default SearchSection;
