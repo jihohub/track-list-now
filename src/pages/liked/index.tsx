366d7b9f46bee30deade5973e34657ba5512c5cb
Jiho Park <diorpark@gmail.com>
2024-10-25 17:22:50 +0900

    Feat: Liked 페이지 추가

---
+++ src/pages/liked/index.tsx
@@ -0,0 +1,11 @@
+const LikedPage = () => {
+  return (
+    <div className="max-w-3xl mx-auto text-white">
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-xl text-neonBlue">This page will be updated soon.</p>
+      </div>
+    </div>
+  );
+};
+
+export default LikedPage;
