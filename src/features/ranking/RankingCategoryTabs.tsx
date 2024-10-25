b7fdc17865f78b17ae3bd42d128a1d5d80916f3d
Jiho Park <diorpark@gmail.com>
2024-10-25 17:18:13 +0900

    Feat: 랭킹 페이지 내에서 카테고리를 변경할 수 있도록 탭 추가

---
+++ src/features/ranking/RankingCategoryTabs.tsx
@@ -0,0 +1,58 @@
+import { useTranslation } from "next-i18next";
+
+interface RankingCategoryTabsProps {
+  category: string;
+  onCategoryChange: (newCategory: string) => void;
+}
+
+const RankingCategoryTabs = ({
+  category,
+  onCategoryChange,
+}: RankingCategoryTabsProps) => {
+  const { t } = useTranslation("common");
+
+  const tabs = [
+    {
+      key: "ALL_TIME_ARTIST",
+      label: t("all_time_favorite_artists"),
+      value: "all-time-artist",
+    },
+    {
+      key: "ALL_TIME_TRACK",
+      label: t("all_time_favorite_tracks"),
+      value: "all-time-track",
+    },
+    {
+      key: "CURRENT_ARTIST",
+      label: t("current_favorite_artists"),
+      value: "current-artist",
+    },
+    {
+      key: "CURRENT_TRACK",
+      label: t("current_favorite_tracks"),
+      value: "current-track",
+    },
+  ];
+
+  return (
+    <div className="flex justify-center space-x-4 mb-6">
+      {tabs.map((tab) => (
+        <button
+          key={tab.key}
+          onClick={() => onCategoryChange(tab.value)}
+          className={`text-xs md:text-lg px-0 md:px-4 py-1 md:py-2 duration-200 
+            ${
+              category === tab.key
+                ? "text-persianBlue font-bold shadow-lg"
+                : " text-gray-300  hover:text-neonBlue"
+            }
+          `}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default RankingCategoryTabs;
